5692ecdc72930a0637db055816b48607cb792118
marianodo <mardom4164@gmail.com>
2024-05-26 16:34:41 -0300

    add remove db

--- api/app/__init__.py
+++ api/app/__init__.py
@@ -4,6 +4,7 @@ from app.extensions import db
 from app.config import config
 from app.blueprints import register_blueprints
 from app.models import *
+import click
 
 app = Flask(__name__)
 CORS(app)
@@ -12,6 +13,13 @@ configuration = config['development']
 # Configuración de la aplicación
 app.config.from_object(configuration)
 
+
+@app.cli.command("drop-db")
+@click.confirmation_option(prompt="Are you sure you want to drop all tables?")
+def drop_db():
+    """Drop all tables in the database."""
+    db.drop_all()
+    click.echo("Dropped all tables in the database.")
 
 with app.app_context():
     db.init_app(app)
